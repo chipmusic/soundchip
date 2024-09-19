660b6d25fa57f4221b7317d0e25afe03c00b7746
chipmusic <chip.music.editor@gmail.com>
2024-09-18 23:56:34 -0700

    Made file saving optional in example

--- example/src/main.rs
+++ example/src/main.rs
@@ -4,12 +4,6 @@ use soundchip::{math::*, prelude::*, presets::*};
 use std::{env::var_os, path::PathBuf};
 
 fn main() -> SdlResult {
-    // I have this path set to a ram disk on my machine,
-    // since I'm saving the wave file for debugging purposes.
-    let target_file: PathBuf = var_os("CARGO_MANIFEST_DIR").unwrap().into();
-    let target_file = target_file.join("target/output.wav");
-    println!("Saving wav file to: {:?}", target_file);
-
     let mut app = App::default()?;
     app.audio_start();
 
@@ -47,13 +41,25 @@ fn main() -> SdlResult {
     }
 
     // Writing in mono for debugging simplicity. Ensure no pan is set in the channel!
+    let target_file: Option<PathBuf> = match var_os("CARGO_MANIFEST_DIR") {
+        Some(os_var) => {
+            let mut dir: PathBuf = os_var.into();
+            Some(dir.join("target/output.wav"))
+        }
+        None => None,
+    };
+    println!("Saving wav file to: {:?}", target_file);
     let wav_spec = WavSpec {
         channels: 1,
         sample_rate: chip.sample_rate,
         bits_per_sample: 16,
         sample_format: hound::SampleFormat::Int,
     };
-    let mut writer = WavWriter::create(target_file, wav_spec).map_err(|e| e.to_string())?;
+    let mut writer = if let Some(target_file) = target_file {
+        Some(WavWriter::create(target_file, wav_spec).map_err(|e| e.to_string())?)
+    } else {
+        None
+    };
 
     // mini_sdl main loop. MiniSDL assumes you want both graphics and sound!
     while !app.quit_requested {
@@ -105,15 +111,21 @@ fn main() -> SdlResult {
                 left: sample.left,
                 right: sample.right,
             });
-            writer
-                .write_sample(i16::from(sample.left))
-                .map_err(|e| e.to_string())?;
+            if let Some(writer) =&mut  writer {
+                writer
+                    .write_sample(i16::from(sample.left))
+                    .map_err(|e| e.to_string())?;
+            }
         }
         drop(audio_input);
 
         app.frame_finish()?;
     }
 
-    writer.finalize().map_err(|e| e.to_string())?;
+    // I have this path set to a ram disk on my machine,
+    // since I'm saving the wave file for debugging purposes.
+    if let Some(writer) = writer {
+        writer.finalize().map_err(|e| e.to_string())?;
+    }
     Ok(())
 }
